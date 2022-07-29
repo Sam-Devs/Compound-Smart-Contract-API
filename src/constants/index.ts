d8fc35e11edada8aafff8c971021fbf42a7fdd17
sam <ayorindesam2@gmail.com>
2022-07-29 10:52:39 +0100

    config

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const PRIVATE_KEY = 'e34a81cb8245347fceafcf582d45c8f4b9797850b96d5a9c5feeddfeee2b79b6';
+export const C_ETH_ADDRESS = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5';
+export const COMPTROLLER_ADDRESS = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b';
+export const PRICE_FEED_ADDRESS = '0x6d2299c48a8dd07a872fdd0f8233924872ad1071';
+export const UNDERLYING_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // Dai
+export const C_TOKEN_ADDRESS = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643'; // cDai
+export const ASSET_NAME = 'DAI'; // for the log output lines
+export const UNDERLYING_DECIMALS = 18; // Number of decimals defined in this ERC20 token's contract
+export const UNDERLYING_TO_BORROW = 50;
+export const ETH_COLATERAL = 0.1; // eth to supply as collateral
+export const WALLET_ADDRESS = '0x02FAa6B6a5Fb58cB4c45B7718C5bBeAB277c4eB6';
+
+// export const PRIVATE_KEY = 'e34a81cb8245347fceafcf582d45c8f4b9797850b96d5a9c5feeddfeee2b79b6';
+// export const C_ETH_ADDRESS = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5';
+// export const COMPTROLLER_ADDRESS = '0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b';
+// export const PRICE_FEED_ADDRESS = '0x6d2299c48a8dd07a872fdd0f8233924872ad1071';
+// export const UNDERLYING_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // Dai
+// export const C_TOKEN_ADDRESS = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643'; // cDai
+// export const ASSET_NAME = 'DAI'; // for the log output lines
+// export const UNDERLYING_DECIMALS = 18; // Number of decimals defined in this ERC20 token's contract
+// export const UNDERLYING_TO_BORROW = 50;
+// export const ETH_COLATERAL = 1; // eth to supply as collateral
