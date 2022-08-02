15dad49d00aab7035f2f38526d2cf687a4a818f8
sam <ayorindesam2@gmail.com>
2022-08-02 10:46:04 +0100

    borrow-with-eth

--- src/handlers/borrow/erc20-with-eth.ts
+++ src/handlers/borrow/erc20-with-eth.ts
@@ -1,16 +1,34 @@
-import { cToken } from "@compound-finance/compound-js/dist/nodejs/api";
 import dotenv from "dotenv";
 dotenv.config();
 const Compound = require("@compound-finance/compound-js");
 const Web3 = require("web3");
 const providerUrl = process.env.PROVIDER_URL;
-const privateKey = process.env.PRIVATE_KEY;
-const web3 = Web3(providerUrl);
+const web3 = new Web3(providerUrl);
 const compound = new (Compound as any)(process.env.PROVIDER_URL, {
-    privateKey: privateKey
-});
-import {Request, Response} from "express";
-import { C_TOKEN_ADDRESS } from "../../constants";
-import { cTokenABI } from "../../abi/cTokenABI";
-import { cErcAbi } from "../../utils/contracts.json";
-import {  } from "../../services/balance";
+    privateKey: process.env.PRIVATE_KEY
+})
+import { Request, Response} from "express";
+import { enterMarket, _balanceOf} from "../../utils";
+
+export const ERC20WithEth = async (req: Request, res: Response) => {
+    try {
+        const { ethToSupplyAsCollateral, assetName, assetToBorrow, walletAddress} = req.body;
+        await compound.supply(Compound.ETH, ethToSupplyAsCollateral);
+        await enterMarket;
+
+        console.log(`Borrowed ${assetToBorrow} ${assetName}`);
+
+        const balance = await _balanceOf(assetName, walletAddress);
+        const ethBalance = await _balanceOf(assetName, walletAddress);
+
+        return res.status(200).send({
+            status: 200,
+            message: "Asset borrowed successfully",
+            borrowBalance: balance,
+            ethBalance: ethBalance
+        })
+        
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
