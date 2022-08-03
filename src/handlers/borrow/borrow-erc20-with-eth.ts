14748af6d3c0eb528046f470eadb3b346e4449ed
sam <ayorindesam2@gmail.com>
2022-08-03 13:06:34 +0100

    updated

--- src/handlers/borrow/borrow-erc20-with-eth.ts
+++ src/handlers/borrow/borrow-erc20-with-eth.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { Request, Response } from "express";
 const Compound = require("@compound-finance/compound-js");
-const Web3 = require("web3");
 const providerUrl = process.env.PROVIDER_URL;
+const Web3 = require("web3");
 const web3 = new Web3(providerUrl);
 const compound = new (Compound as any)(process.env.PROVIDER_URL, {
   privateKey: process.env.PRIVATE_KEY,
 });
-import { Request, Response } from "express";
 import { enterMarket, _balanceOf } from "../../utils";
 
 export const BorrowErc20WithEth = async (req: Request, res: Response) => {
@@ -16,22 +16,22 @@ export const BorrowErc20WithEth = async (req: Request, res: Response) => {
       ethToSupplyAsCollateral,
       underlyingToBorrow,
       assetName,
-      myWalletAddress,
+      walletAddress,
     } = req.body;
 
     await compound.supply(Compound.ETH, ethToSupplyAsCollateral);
     await enterMarket();
 
     console.log(
-      `Now attempting to borrow ${underlyingToBorrow} ${assetName}...`
+      `Now attempting to borrow ${underlyingToBorrow} ${assetName}`
     );
 
     let tx = await compound.borrow(assetName, underlyingToBorrow);
     await tx.wait(1);
-    console.log(`Borrowed ${underlyingToBorrow} ${assetName}...`);
+    console.log(`Borrowed ${underlyingToBorrow} ${assetName}`);
 
-    const balance = await _balanceOf(assetName, myWalletAddress);
-    const ethBalance = await _balanceOf(assetName, myWalletAddress);
+    const balance = await _balanceOf(assetName, walletAddress);
+    const ethBalance = await _balanceOf(assetName, walletAddress);
 
     return res.status(200).send({
       status: 200,
