8f025c867b5aa62271049a803b1016b6bc5ca4cd
sam <ayorindesam2@gmail.com>
2022-08-03 13:17:48 +0100

    updated

--- src/handlers/supply/redeem.ts
+++ src/handlers/supply/redeem.ts
@@ -1,9 +1,8 @@
-import { Response, Request } from "express";
-
 import dotenv from "dotenv";
 dotenv.config();
 
-// import { abiJson } from "../../abi/ethAbi";
+import { Response, Request } from "express";
+
 import { kovanABI } from "../../abi/kovanAbi";
 
 const Web3 = require("web3");
@@ -16,40 +15,41 @@ const privateKey = process.env.PRIVATE_KEY;
 web3.eth.accounts.wallet.add("0x" + privateKey);
 
 // Main Net Contract for cETH (the supply process is different for cERC20 tokens)
-const contractAddress = "a";
+const contractAddress = "0x41b5844f4680a8c38fbb695b7f9cfd1f64474a72";
 const cEthContract = new web3.eth.Contract(kovanABI, contractAddress);
 
-const ethDecimals = 18; // Ethereum has 18 decimal places
+// Ethereum has 18 decimal places
+const ethDecimals = 18; 
 
 export const redeemToken = async (req: Request, res: Response) => {
   try {
-    const { amountToRedeem, myWalletAddress } = req.body;
+    const { amountToRedeem, walletAddress } = req.body;
     let exchangeRateCurrent =
       (await cEthContract.methods.exchangeRateCurrent().call()) /
       Math.pow(10, 18 + ethDecimals - 8);
 
     let cTokenBalance =
-      (await cEthContract.methods.balanceOf(myWalletAddress).call()) / 1e8;
+      (await cEthContract.methods.balanceOf(walletAddress).call()) / 1e8;
 
     const balanceOfUnderlying =
       (await await web3.utils.toBN(
-        await cEthContract.methods.balanceOfUnderlying(myWalletAddress).call()
+        await cEthContract.methods.balanceOfUnderlying(walletAddress).call()
       )) / Math.pow(10, ethDecimals);
 
     // Redeeming
 
     //Replace balance of underlying with cTokenBalance
     await cEthContract.methods.redeem(cTokenBalance * 1e8).send({
-      from: myWalletAddress,
+      from: walletAddress,
       gasLimit: web3.utils.toHex(500000),
       gasPrice: web3.utils.toHex(20000000000), // use ethgasstation.info (mainnet only)
     });
 
     const ethBalance =
-      (await web3.eth.getBalance(myWalletAddress)) / Math.pow(10, ethDecimals);
+      (await web3.eth.getBalance(walletAddress)) / Math.pow(10, ethDecimals);
     return res.status(200).send({
       status: "success",
-      message: "Redeem Successfully",
+      message: "Asset Redeemed Successfully",
       currentExchangeRate: exchangeRateCurrent,
       balanceOfUnderlying: balanceOfUnderlying,
       ethBalance: ethBalance,
