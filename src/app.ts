ddecef41d08006d39877450e482f1a178b704d3c
sam <ayorindesam2@gmail.com>
2022-08-03 13:29:34 +0100

    updated

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, { Response, Request } from "express";
+const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/routes";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+dotenv.config();
+
+app.use(cors());
+app.use((req: Request, res: Response, next: any) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Content-type", "application/json");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// route
+app.use("/api", router);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send({ status: 200, message: "server up and running" });
+});
+
+const port: string | number = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  return console.log(`Server is running on port ${port}`);
+});
